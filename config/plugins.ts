f348e1f799b485f31f47d808f3a0468a6631d697
Kaiser Tran <khtran@axon.com>
2025-02-14 20:01:27 +0700

    init commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    slugify: {
+        enabled: true,
+        config: {
+            contentTypes: {
+                blog: {
+                    field: 'slug',
+                    references: 'title',
+                    slugifyWithCount: true,
+                    shouldUpdateSlug: true,
+                    skipUndefinedReferences: false,
+                },
+            },
+        },
+    },
+});
