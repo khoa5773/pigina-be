dfd0ccc30672b2210d6a53a59f156127a5df7f63
minh.nghiem <minh.nghiem@mservice.com.vn>
2025-02-24 23:30:32 +0700

    add slug api product

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -473,7 +473,8 @@ export interface ApiFaqFaq extends Struct.CollectionTypeSchema {
 export interface ApiProductProduct extends Struct.CollectionTypeSchema {
   collectionName: 'products';
   info: {
-    displayName: 'product';
+    description: '';
+    displayName: 'Product';
     pluralName: 'products';
     singularName: 'product';
   };
@@ -502,6 +503,7 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
       Schema.Attribute.Private;
     name: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
+    slug: Schema.Attribute.String;
     tooltip: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
