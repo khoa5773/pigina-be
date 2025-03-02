0848740ed23c562886f8bd80aa90be737985549e
minh.nghiem <minh.nghiem@mservice.com.vn>
2025-03-02 21:45:24 +0700

    fix slug api

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,16 +1,33 @@
 module.exports = ({ env }) => ({
-    slugify: {
-        enabled: true,
-        config: {
-            contentTypes: {
-                blog: {
-                    field: 'slug',
-                    references: 'title',
-                    slugifyWithCount: true,
-                    shouldUpdateSlug: true,
-                    skipUndefinedReferences: false,
-                },
-            },
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        blog: {
+          field: "slug",
+          references: "title",
+          slugifyWithCount: true,
+          shouldUpdateSlug: true,
+          skipUndefinedReferences: false,
         },
+        product: {
+          field: "slug",
+          references: "title",
+          slugifyWithCount: true,
+          shouldUpdateSlug: true,
+          skipUndefinedReferences: false,
+        },
+        faq: {
+          field: "slug",
+          references: "title",
+          slugifyWithCount: true,
+          shouldUpdateSlug: true,
+          skipUndefinedReferences: false,
+        },
+      },
     },
-});
+  },
+  "media-prefix": {
+    enabled: true,
+  },
+})
